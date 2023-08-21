8f80111a492ce4af7870d6eafa0e1d699c6f020d
sivakrishna <sivakrishn17@gmail.com>
2023-08-22 00:36:50 +0530

    hello world script added

---
+++ 13-functions.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Our program goal is to install Mysql
+
+USERID=$(id -u)
+
+  # this function should validate the previous command and it inform user success or failure 
+ VALIDATE (){
+   #$1 it will receive the input argument1
+   if [ $1 -ne 0 ]
+   then 
+      echo "installation........ FAILURE"
+      exit 1
+else
+    echo "installation........... SUCCESS" 
+fi
+
+ }
+ 
+ if [ $USERID -ne 0 ]
+ then
+ echo "ERROR:please run this script with root access"
+ exit 1
+ # else 
+ # echo "INFO: you are root user"
+
+fi
+
+#it is our responsibility again to check installation is success or not
+   yum install mysql  -y
+
+   VALIDATE $?
+
+   yum install postfix  -y
+   
+   VALIDATE $?
+
