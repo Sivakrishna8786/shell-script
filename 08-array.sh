a94f1a725226ac258dfe72b2c1c9a70dee93e8fa
sivakrishna <sivakrishn17@gmail.com>
2023-08-21 22:26:42 +0530

    hello world script added

--- 08-array.sh
+++ 08-array.sh
@@ -5,4 +5,10 @@ PERSONS=("Ramesh" "Suresh" "SACHINE")
 
 #inside array we always refer values with index and its start from 0
 
-echo "Frist person of: ${PERSONS[@]}"
+#Peron to person
+echo "Frist person of: ${PERSONS[0]}"
+echo "Second person of: ${PERSONS[1]}"
+echo "Third person of: ${PERSONS[2]}"
+
+# For all persons to print below one is the syntax
+echo "All person of: ${PERSONS[@]}"
