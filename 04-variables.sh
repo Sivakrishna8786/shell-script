f703f6368c038e4035bc4a6988aeaff911bdfb80
sivakrishna <sivakrishn17@gmail.com>
2023-08-21 21:06:36 +0530

    hello world script added

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#this is a command, we need to develop to adding of 2 numbers
+
+NUMBER1=$1
+NUMBER2=$2
+
+# I need to run a command inside shell to add these 2 numbers
+ SUM=$((NUMBER1+NUMBER2))
+
+ 
