c20c0514d5430ea10b2d4397294b81e9e81197a4
sivakrishna <sivakrishn17@gmail.com>
2023-08-21 20:48:33 +0530

    hello world script added

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,5 +3,4 @@
 #shell script run the \command inside $() and stores the variable in to outside as date
 Date=$(date)
 
-echo "Hello, this script is executed as timestamp: $Date.
-
+echo "Hello, this script is executed as timestamp: $Date"
