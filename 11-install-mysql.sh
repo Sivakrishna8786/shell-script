9d70fa299c7d0f4eaa721fd448209fad85bb4fb8
sivakrishna <sivakrishn17@gmail.com>
2023-08-22 00:07:02 +0530

    hello world script added

--- 11-install-mysql.sh
+++ 11-install-mysql.sh
@@ -13,4 +13,15 @@ USERID=$(id -u)
 
 fi
 
-   yum install mysql  -y
+#it is our responsibility again to check installation is success or not
+   yum install mysqlll  -y
+
+   if [ $? -ne 0 ]
+   then 
+      echo "installation of mysql is error"
+      exit 1
+
+    echo "installation is mysql is success" 
+fi
+
+
