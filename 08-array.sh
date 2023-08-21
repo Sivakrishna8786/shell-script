181b583925ddaf1eaa156694b90e71b4e1811ad5
sivakrishna <sivakrishn17@gmail.com>
2023-08-21 22:18:54 +0530

    hello world script added

---
+++ 08-array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# this is ARRAY - it Can hold multiple values instead of a single value
+PERSONS=("Ramesh" "Suresh" "SACHINE")
+
+#inside array we always refer values with index and its start from 0
+echo "Frist person of: ${PERSON1[0]}"
