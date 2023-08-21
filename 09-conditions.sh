8b0f03218600f867dc232568e79703bbd7715186
sivakrishna <sivakrishn17@gmail.com>
2023-08-21 23:13:08 +0530

    hello world script added

---
+++ 09-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# check if the number is greather than 10 or not
+NUMBER=$1
+if [$NUMBER-gt 10 ]
+then
+
+echo "$NUMBER is greather than 10"
+
+else 
+
+echo "$NUMBER is not greather than 10"
+
+fi
