a19aac781b6f099959ffd18ab178be1d093fc08d
sivakrishna <sivakrishn17@gmail.com>
2023-08-22 12:07:39 +0530

    hello world script added

--- 13-functions.sh
+++ 13-functions.sh
@@ -12,8 +12,6 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-
-
   # this function should validate the previous command and it inform user success or failure 
  VALIDATE (){
    #$1 it will receive the input argument1
