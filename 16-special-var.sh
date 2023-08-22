13a24b64050ede6ab3898c546e900d715d4d3429
sivakrishna <sivakrishn17@gmail.com>
2023-08-22 11:03:32 +0530

    hello world script added

--- 16-special-var.sh
+++ 16-special-var.sh
@@ -4,5 +4,4 @@ VAR1=$1
 VAR2=$2
 
 echo "variable 1: $VAR1"
-
-
+echo "all variables: $@"
--- 17-special-var.sh
+++ 17-special-var.sh
@@ -4,5 +4,5 @@ VAR1=$1
 VAR2=$2
 
 echo "variable 1: $VAR1"
-
-
+echo "all variables: $@"
+echo "mber of vars: $#"
