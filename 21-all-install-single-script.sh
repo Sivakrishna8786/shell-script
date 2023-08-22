422717cbbcaca60e238d3129d184623b79d85b99
sivakrishna <sivakrishn17@gmail.com>
2023-08-22 17:12:30 +0530

    hello world script added

--- 21-all-install-single-script.sh
+++ 21-all-install-single-script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-$USERID=[id -u]
+USERID=$(id -u)
 R="\e[31m"
 N="\e[0m"
-if [$USERID -ne 0]
+
+if [$USERID -ne 0];
 then 
     echo -e "$R ERROR::please run thise script with root user $N"
     exit 1
