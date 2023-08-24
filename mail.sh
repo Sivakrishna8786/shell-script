2d7c32514476abfb6af34837b1f95b8782e72731
sivakrishna <sivakrishn17@gmail.com>
2023-08-25 00:11:28 +0530

    hello world script added

--- mail.sh
+++ mail.sh
@@ -11,4 +11,4 @@ ALERT_TYPE=$5
 
 #echo "All args: $@"
  FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html) 
-echo "$BODY" | mail -s "$SUBJECT" "$TO_ADDRESS"
+echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContect-type: text/html")" "$TO_ADDRESS"
